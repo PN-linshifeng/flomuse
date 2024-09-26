bf60faa94be616f46f95de3235b8399be75b1fef
“pn” <245810159@qq.com>
2024-09-26 13:31:56 +0800

    dev

--- src/components/buy/index.vue
+++ src/components/buy/index.vue
@@ -1,13 +1,15 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 const active = ref('b')
+const select = ref<'月' | '季' | '年'>('季')
+const beihe = ref<10 | 110 | 1100 | 11100>(1100)
 </script>
 <template>
   <div
-    class="absolute left-0 right-0 top-0 w-[100vw] h-[100vh] overflow-hidden flex items-center justify-center"
+    class="absolute left-[-1px] right-0 top-[-1px] w-[100vw] h-[100vh] overflow-hidden flex items-center justify-center"
   >
     <div
-      class="w-[1200px] rounded-[30px] shadow bg-theme overflow-hidden"
+      class="w-[1200px] rounded-[30px] bg-theme overflow-hidden shadow-2xl"
       style="background: linear-gradient(to bottom, #e1efc7, #818972)"
     >
       <header class="h-[100px] relative">
@@ -33,11 +35,11 @@ const active = ref('b')
         </div>
         <div class="text-center text-3xl leading-[100px] text-black">请选择您的付费方式</div>
       </header>
-      <div class="h-[90px] leading-[90px] relative">
+      <div class="h-[90px] leading-[90px] relative overflow-hidden">
         <div class="w-full h-[80px] absolute top-[10px] bg-[#B1DCCC] rounded-t-[30px]"></div>
         <div class="flex text-[#06452E] *:flex-1 *:relative *:z-10 *:text-center">
           <div class="" :class="active === 'a' ? 'active' : ''" @click="active = 'a'">
-            <div class="text-2xl leading-[90px] cursor-pointer">订阅制会员</div>
+            <div class="text-2xl leading-[100px] cursor-pointer">订阅制会员</div>
             <div class="left">
               <div class="tabs"></div>
             </div>
@@ -46,7 +48,7 @@ const active = ref('b')
             </div>
           </div>
           <div :class="active === 'b' ? 'active' : ''" @click="active = 'b'">
-            <div class="text-2xl leading-[90px] cursor-pointer">贝壳充值</div>
+            <div class="text-2xl leading-[100px] cursor-pointer">贝壳充值</div>
             <div class="left">
               <div class="tabs"></div>
             </div>
@@ -58,8 +60,8 @@ const active = ref('b')
       </div>
       <div class="bg-white pb-6 pt-[70px] px-12">
         <!-- 订阅制会员 -->
-        <div class="flex">
-          <div>
+        <div class="flex" v-show="active === 'a'" key="a">
+          <div class="mr-16 w-[300px]">
             <div class="text-[22px] mb-5"><span class="text-yellow">订购权益</span>包括：</div>
             <div class="text-base *:flex *:items-center *:mb-8">
               <div class="">
@@ -86,20 +88,43 @@ const active = ref('b')
           </div>
           <div class="flex-1">
             <div
-              class="flex gap-7 text-base text-center text-black *:flex-1 *:flex *:justify-center *:flex-col *:border *:rounded-xl *:h-[174px]"
+              class="flex gap-7 text-base text-center text-black *:flex-1 *:flex *:justify-center *:flex-col *:border *:rounded-xl *:h-[174px] *:cursor-pointer *:relative"
             >
-              <div class="*:my-2">
+              <div
+                class="*:my-2"
+                :class="select === '月' ? ' border-yellow bg-[#FEF9F8]' : ''"
+                @click="select = '月'"
+              >
+                <div
+                  v-show="select === '月'"
+                  class="text-white !m-0 absolute left-[-1px] top-[-1px] translate-y-[-50%] h-8 w-[45%] rounded-full rounded-bl-[0px] text-sm leading-8 bg-gradient-to-r from-yellow to-[#FE8E3D]"
+                >
+                  最多选择
+                </div>
                 <div>季度会员</div>
                 <div>
                   <del>¥108/<span class="text-hui">月</span></del>
                 </div>
                 <div>
-                  <span class="px-8 py-2 rounded-full text-[20px] bg-[#F5F5F5] inline-block"
+                  <span
+                    class="px-8 py-2 rounded-full text-[20px] bg-[#F5F5F5] inline-block"
+                    :class="select === '月' ? ' text-yellow bg-[#FFE2DC]' : ''"
                     >限时免费</span
                   >
                 </div>
               </div>
-              <div class="*:my-2">
+
+              <div
+                class="*:my-2"
+                :class="select === '季' ? ' border-yellow bg-[#FEF9F8]' : ''"
+                @click="select = '季'"
+              >
+                <div
+                  v-show="select === '季'"
+                  class="text-white !m-0 absolute left-[-1px] top-[-1px] translate-y-[-50%] h-8 w-[45%] rounded-full rounded-bl-[0px] text-sm leading-8 bg-gradient-to-r from-yellow to-[#FE8E3D]"
+                >
+                  最多选择
+                </div>
                 <div>季度会员</div>
                 <div>
                   <del>
@@ -107,12 +132,24 @@ const active = ref('b')
                   </del>
                 </div>
                 <div>
-                  <span class="px-8 py-2 rounded-full text-[20px] bg-[#F5F5F5] inline-block"
+                  <span
+                    class="px-8 py-2 rounded-full text-[20px] bg-[#F5F5F5] inline-block"
+                    :class="select === '季' ? ' text-yellow bg-[#FFE2DC]' : ''"
                     >限时免费</span
                   >
                 </div>
               </div>
-              <div class="*:my-2">
+              <div
+                class="*:my-2"
+                :class="select === '年' ? ' border-yellow bg-[#FEF9F8]' : ''"
+                @click="select = '年'"
+              >
+                <div
+                  v-show="select === '年'"
+                  class="text-white !m-0 absolute left-[-1px] top-[-1px] translate-y-[-50%] h-8 w-[45%] rounded-full rounded-bl-[0px] text-sm leading-8 bg-gradient-to-r from-yellow to-[#FE8E3D]"
+                >
+                  最多选择
+                </div>
                 <div>年度会员</div>
                 <div>
                   <del>
@@ -120,10 +157,203 @@ const active = ref('b')
                   </del>
                 </div>
                 <div>
-                  <span class="px-8 py-2 rounded-full text-[20px] bg-[#F5F5F5] inline-block"
+                  <span
+                    class="px-8 py-2 rounded-full text-[20px] bg-[#F5F5F5] inline-block"
+                    :class="select === '年' ? ' text-yellow bg-[#FFE2DC]' : ''"
                     >限时免费</span
                   >
                 </div>
+              </div>
+            </div>
+            <!-- 二维码 -->
+            <div class="px-4 py-7 text-center bg-[#F5F5F5] mt-8 rounded-[32px] text-base">
+              <div class="mb-7">应付金额：<span class="text-yellow">0</span> 元</div>
+              <div class="flex justify-center px-20 *:flex-1">
+                <div>
+                  <div class="flex justify-center">
+                    <div class="border border-[#48B338] p-2 mb-4 w-[120px] h-[120px] rounded-xl">
+                      <img src="@/assets/images/code.png" alt="" srcset="" />
+                    </div>
+                  </div>
+                  <div class="flex items-center w-full justify-center">
+                    <img
+                      src="@/assets/images/wxp.png"
+                      class="size-5 mr-2"
+                      alt=""
+                      srcset=""
+                    />微信支付
+                  </div>
+                </div>
+                <div>
+                  <div class="flex justify-center">
+                    <div class="border border-[#2EA7E0] p-2 mb-4 w-[120px] h-[120px] rounded-xl">
+                      <img src="@/assets/images/code.png" alt="" srcset="" />
+                    </div>
+                  </div>
+                  <div class="flex items-center w-full justify-center">
+                    <img
+                      src="@/assets/images/zyp.png"
+                      class="size-5 mr-2"
+                      alt=""
+                      srcset=""
+                    />微信支付
+                  </div>
+                </div>
+              </div>
+              <div class="mt-10">
+                支付即视为同意并阅读<a href="#" class="text-black">《会员服务协议》</a>和<a
+                  href="#"
+                  class="text-black"
+                  >《会员详细规则》</a
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- 贝壳充值 -->
+        <div class="relative" v-show="active === 'b'" key="b">
+          <div class="absolute translate-y-[-150%] text-lg mb-10">
+            灵活充值，按需购买，让您的每一笔花费都物有所值
+          </div>
+          <div
+            class="flex gap-7 text-base text-center text-black *:flex-1 *:flex *:justify-center *:flex-col *:border *:rounded-xl *:h-[174px] *:cursor-pointer *:relative *:p-6"
+          >
+            <div
+              class="*:my-2"
+              :class="beihe === 10 ? ' border-yellow bg-[#FEF9F8]' : ''"
+              @click="beihe = 10"
+            >
+              <div
+                v-show="beihe === 10"
+                class="text-white !m-0 absolute left-[-1px] top-[-1px] translate-y-[-50%] h-8 w-[45%] rounded-full rounded-bl-[0px] text-sm leading-8 bg-gradient-to-r from-yellow to-[#FE8E3D]"
+              >
+                最多选择
+              </div>
+              <div>10虚拟币包</div>
+              <div class="text-2xl font-[600]">¥1.1</div>
+              <div>
+                <span
+                  class="w-full px-1 py-2 rounded-full text-sm bg-[#F5F5F5] inline-block"
+                  :class="beihe === 10 ? ' text-yellow bg-[#FFE2DC]' : ''"
+                  >每日首次购买0.3折 0.33元</span
+                >
+              </div>
+            </div>
+
+            <div
+              class="*:my-2"
+              :class="beihe === 110 ? ' border-yellow bg-[#FEF9F8]' : ''"
+              @click="beihe = 110"
+            >
+              <div
+                v-show="beihe === 110"
+                class="text-white !m-0 absolute left-[-1px] top-[-1px] translate-y-[-50%] h-8 w-[45%] rounded-full rounded-bl-[0px] text-sm leading-8 bg-gradient-to-r from-yellow to-[#FE8E3D]"
+              >
+                最多选择
+              </div>
+              <div>110虚拟币包</div>
+              <div class="text-2xl font-[600]">¥9.9</div>
+              <div>
+                <span
+                  class="w-full px-1 py-2 rounded-full text-sm bg-[#F5F5F5] inline-block"
+                  :class="beihe === 110 ? ' text-yellow bg-[#FFE2DC]' : ''"
+                  >10%OFF</span
+                >
+              </div>
+            </div>
+            <div
+              class="*:my-2"
+              :class="beihe === 1100 ? ' border-yellow bg-[#FEF9F8]' : ''"
+              @click="beihe = 1100"
+            >
+              <div
+                v-show="beihe === 1100"
+                class="text-white !m-0 absolute left-[-1px] top-[-1px] translate-y-[-50%] h-8 w-[45%] rounded-full rounded-bl-[0px] text-sm leading-8 bg-gradient-to-r from-yellow to-[#FE8E3D]"
+              >
+                最多选择
+              </div>
+              <div>1100虚拟币包</div>
+              <div class="text-2xl font-[600]">¥89</div>
+              <div>
+                <span
+                  class="w-full px-1 py-2 rounded-full text-sm bg-[#F5F5F5] inline-block"
+                  :class="beihe === 1100 ? ' text-yellow bg-[#FFE2DC]' : ''"
+                  >20%OFF</span
+                >
+              </div>
+            </div>
+            <div
+              class="*:my-2"
+              :class="beihe === 11100 ? ' border-yellow bg-[#FEF9F8]' : ''"
+              @click="beihe = 11100"
+            >
+              <div
+                v-show="beihe === 11100"
+                class="text-white !m-0 absolute left-[-1px] top-[-1px] translate-y-[-50%] h-8 w-[45%] rounded-full rounded-bl-[0px] text-sm leading-8 bg-gradient-to-r from-yellow to-[#FE8E3D]"
+              >
+                最多选择
+              </div>
+              <div>11100虚拟币包</div>
+              <div class="text-2xl font-[600]">¥848</div>
+              <div>
+                <span
+                  class="w-full px-1 py-2 rounded-full text-sm bg-[#F5F5F5] inline-block"
+                  :class="beihe === 11100 ? ' text-yellow bg-[#FFE2DC]' : ''"
+                  >30%OFF</span
+                >
+              </div>
+            </div>
+          </div>
+          <div class="flex mt-8 mr-16">
+            <div class="w-[300px]">
+              <div class="text-[22px] mb-5"><span class="text-yellow">常见问题</span>解答：</div>
+              <div class="text-base *:flex *:items-center *:mb-8">
+                <div class="">贝壳是否有有效期？</div>
+                <div class="">充值多久后贝壳可以到账？</div>
+                <div class="">我可以在哪里看到我的购买记录？</div>
+              </div>
+            </div>
+            <!-- 二维码 -->
+            <div class="flex-1 px-4 py-7 text-center bg-[#F5F5F5] rounded-[32px] text-base">
+              <div class="mb-7">应付金额：<span class="text-yellow">0</span> 元</div>
+              <div class="flex justify-center px-20 *:flex-1">
+                <div>
+                  <div class="flex justify-center">
+                    <div class="border border-[#48B338] p-2 mb-4 w-[120px] h-[120px] rounded-xl">
+                      <img src="@/assets/images/code.png" alt="" srcset="" />
+                    </div>
+                  </div>
+                  <div class="flex items-center w-full justify-center">
+                    <img
+                      src="@/assets/images/wxp.png"
+                      class="size-5 mr-2"
+                      alt=""
+                      srcset=""
+                    />微信支付
+                  </div>
+                </div>
+                <div>
+                  <div class="flex justify-center">
+                    <div class="border border-[#2EA7E0] p-2 mb-4 w-[120px] h-[120px] rounded-xl">
+                      <img src="@/assets/images/code.png" alt="" srcset="" />
+                    </div>
+                  </div>
+                  <div class="flex items-center w-full justify-center">
+                    <img
+                      src="@/assets/images/zyp.png"
+                      class="size-5 mr-2"
+                      alt=""
+                      srcset=""
+                    />微信支付
+                  </div>
+                </div>
+              </div>
+              <div class="mt-10">
+                支付即视为同意并阅读<a href="#" class="text-black">《会员服务协议》</a>和<a
+                  href="#"
+                  class="text-black"
+                  >《会员详细规则》</a
+                >
               </div>
             </div>
           </div>
